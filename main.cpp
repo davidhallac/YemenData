49ae9494f8d75e105f60775a8d284561dc982d2a
David Hallac <david.hallac@gmail.com>
2014-01-19 15:22:49 -0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  YemenData
+//
+//  Created by David Hallac on 1/18/14.
+//  Copyright (c) 2014 David Hallac. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include "/Users/Hallac/Desktop/Snap-2.1/snap-core/Snap.h"
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+        
+}
+
