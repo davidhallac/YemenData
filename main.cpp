d53cd5922871ebff3b106fdbd1e4ea03976ccf77
David Hallac <david.hallac@gmail.com>
2014-01-27 11:58:07 -0800

    Bug Fixes

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ int main(int argc, const char * argv[])
 		{
 			//To see how fast the code is running
 			counter = counter + 1;
-			if (counter % 10000/*1000000*/ == 1)
+			if (counter % 1000000 == 1)
 				cout << counter << ", " << Ss.GetFld(9) << "\n";		
 			
 			TPhoneCall call;
