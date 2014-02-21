2bc2f6a647edcbc8cbc01e0266a039f69c0ac91e
David Hallac <david.hallac@gmail.com>
2014-02-20 23:04:48 -0800

    reliability tests

--- process.cpp
+++ process.cpp
@@ -2,8 +2,7 @@
 //  main.cpp
 //  YemenData
 //
-//  Created by David Hallac on 1/18/14.
-//  Copyright (c) 2014 David Hallac. All rights reserved.
+//
 //
 
 #include <iostream>
@@ -13,7 +12,7 @@
 #include <string>
 #include <cmath>
 #include "../../snap-core/Snap.h"
-//#include "csv_v3.h"
+//#include "csv_v3.h" - don't use this anymore
 #include "process.h"
 
 using namespace std;
@@ -40,6 +39,7 @@ int main(int argc, const char * argv[])
 		//Find Day of call
 		TInt day = (TStr(Ss.GetFld(9)).GetSubStr(0,7).GetInt());
 		
+		//Break monthly file into day-by-day
 		while( !Ss.Eof() && ((TStr(Ss.GetFld(9)).GetSubStr(0,7).GetInt())) == day)
 		{
 			//To see how fast the code is running
@@ -55,7 +55,7 @@ int main(int argc, const char * argv[])
 			TStr locsrc;
 			TStr locdest;
 
-			if(Ss.GetInt(8) == 1)
+			if(Ss.GetInt(8) == 1) //Sent call
 			{
 				//Source ID
 				if(Ss.IsInt(0))
@@ -67,32 +67,31 @@ int main(int argc, const char * argv[])
 				if(Ss.IsInt(5))
 				{
 					TUInt64 temp = (TStr(Ss.GetFld(5))).GetUInt64();
-					//if(temp == 111 || temp == 8888 || temp == 7312345 || temp = 8889)
 					if(temp < 1000000 || temp == 7312345 || temp == 96773100004)
 						badcall = 1;
 					else
 					{
 						dest = (TStr(Ss.GetFld(5))).GetUInt64();
 						if (dest < 1000000000 && dest > 10000000)
-							dest = dest + 967000000000;
+							dest = dest + 967000000000; //Add country code
 				}	}
 				else
-				{ //About 1 error like this per 25,000 calls. Just ignore these
+				{ //About 1 error like this per 25,000 calls
 					badcall = 1;
 				}	
 
 				//Source Location
 				locsrc = Ss.GetFld(3); //Has numbers/letters
 
-				//Destination Location???
-				locdest = "";//Doesn't exist
+				//Destination Location doesn't exist
+				locdest = "";
 			}
-			else if(Ss.GetInt(8) == 2)
+			else if(Ss.GetInt(8) == 2) //Received call
 			{
+				//Source ID (note everything is swapped from above case)
 				if(Ss.IsInt(5))
 				{
 					TUInt64 temp = (TStr(Ss.GetFld(5))).GetUInt64();
-					//if(temp == 111 || temp == 8888 || temp == 7312345 || temp = 8889)
 					if(temp < 1000000 || temp == 7312345 || temp == 96773100004)
 						badcall = 1;
 					else
@@ -103,10 +102,11 @@ int main(int argc, const char * argv[])
 					}
 				}
 				else
-				{ //About 1 error like this per 25,000 calls. Just ignore these
+				{
 					badcall = 1;
 				}
 
+				//Destination ID
 				if(Ss.IsInt(0))
 					dest = (TStr(Ss.GetFld(0))).GetUInt64();
 				else
@@ -126,25 +126,27 @@ int main(int argc, const char * argv[])
 			durfield.DelChAll('.');
 			TInt duration = (durfield).GetInt(); //Sometimes has a "." at the end
 			
-			//Time of Call
+			//Time of Call - YYYYMMDDHHMMSS
 			TInt starttime;
 			if(TStr(Ss.GetFld(9)).Len() >= 14)
 				starttime = (TStr(Ss.GetFld(9)).GetSubStr(8)).GetInt();
 			else
 				badcall = 1;
 
+			//Set TPhoneCall values
 			call.setVals(src, dest, locsrc, locdest, duration, starttime);
 			
+			//If it's not a bad call, add it to the vector
 			if(!badcall)
 			{
-				//Hash Table:
+				//Hash Table to see if src has made previous call
 				if(repeatCalls.IsKey(src))	
 				{
-					//Check if most recent call was same one
+					//Check if the call in the hash table was the reciprocal call
 					if(PhoneV.Len() > 0 && abs(repeatCalls.GetDat(src).getDuration() - duration) < 5 && abs(repeatCalls.GetDat(src).getTime() - starttime) < 5 && repeatCalls.GetDat(src).getDest() == dest)
 					{
-						badcall = 1;
-						//cout << counter << "\n";
+						//It is a repeat. Don't add to vector, simply update src/dest location
+						badcall = 1; 
 						if(Ss.GetInt(8) == 1)
 						{
 							if(PhoneV.Last().getSource() == src && PhoneV.Last().getDest() == dest)
@@ -157,7 +159,7 @@ int main(int argc, const char * argv[])
 								//Unlucky (< 0.5% of repeats). No way but to start searching
 								int sanitytest = 0;
 								int elements = PhoneV.Len()-1;
-								//Increase this value to miss NO results
+								//Increase this value to miss NO results - but slower
 								while(sanitytest < 100000 && elements >= 0)
 								{
 									if(PhoneV[elements].getSource() == src && PhoneV[elements].getDest() == dest)
@@ -168,13 +170,12 @@ int main(int argc, const char * argv[])
 									sanitytest++;
 									elements--;
 								}
-								//if(sanitytest == 1000000 || elements == -1)
-								//	cout << "Missed element #" << counter << "\n";
-							}			
+							}	
+							//No need to update repeatCalls, since this call won't be repeated again...		
 						}
 						else
 						{
-							//Need to fill in LocDest
+							//Fill in LocDest
 							if(PhoneV.Last().getSource() == src && PhoneV.Last().getDest() == dest)
 							{
 								//Vast majority of repeats are one behind
@@ -185,7 +186,7 @@ int main(int argc, const char * argv[])
 								//Unlucky (< 0.5% of repeats). No way but to start searching
 								int sanitytest = 0;
 								int elements = PhoneV.Len()-1;
-								//Increase this value to miss NO results
+								//Increase this value to miss NO results - but a lot slower
 								while(sanitytest < 100000 && elements >= 0)
 								{
 									if(PhoneV[elements].getSource() == src && PhoneV[elements].getDest() == dest)
@@ -196,12 +197,11 @@ int main(int argc, const char * argv[])
 									sanitytest++;
 									elements--;
 								}
-								//if(sanitytest == 1000000 || elements == -1)
-								//	cout << "Missed element #" << counter << "\n";
 							}
 						}
 					}
 					else
+						//Otherwise, simply add this call to the list of previous calls
 						repeatCalls.AddDat(src,call);
 
 				}	
@@ -211,7 +211,7 @@ int main(int argc, const char * argv[])
 				}
 				
 				
-
+				//If a valid call, add to vector, and move on to next one
 				if(!badcall)
 					PhoneV.Add(call);
 			}
@@ -219,52 +219,15 @@ int main(int argc, const char * argv[])
 			Ss.Next();
 
 		}
+		//Done with one day, save it to a file.
 		cout << day << " " << (TStr(Ss.GetFld(9)).GetSubStr(0,7).GetInt()) << ", " << PhoneV.Len() << "\n";
 		TFOut fout(argv[2] + day.GetStr());
-		if(PhoneV.Len() > 100)
+		if(PhoneV.Len() > 100) //Denoising
 			PhoneV.Save(fout);
 
 	}
 
-
-	//load/save code. Seems to be working
-	//TVec<TPhoneCall> PhoneLoad;
-	//TFIn fin("20100510"); 
-	//PhoneLoad.Load(fin);
- 	//cout << PhoneLoad[1].getTime() << "\n";
-
-
-
-	//NON-SNAP METHOD
-    /*ifstream in;
-    in.open("TestData.csv");
-	if (!in.is_open())
-	{
-		cout << "Could not open file\n";
-		return 0;
-	}*/
-	//io::CSVReader<3, io::trim_chars<' '>, io::no_quote_escape<','>> in("TestData.csv");
-  	//in.read_header(io::ignore_extra_column, "vendor", "size", "speed");
-  	//std::string vendor; int size; double speed;
-  	//while(in.read_row(vendor, size, speed)){
-    // do stuff with the data
-  		//cout << vendor << "\n";
-  	//}
-	//Create new graph
-	/*PNEGraph G = PNEGraph::TObj::New();
-	string phonecall;
-	while (getline(in,phonecall))
-	{
-		cout << phonecall << "\n";
-
-		//For each edge, add it to the graph
-		char delimiter = ',';
-		string token = phonecall.substr(0, phonecall.find(delimiter));
-		cout << token << "\n";
-	}
-	in.close();*/
-
-    cout << "Hello, World!\n";
+    cout << "Finished!\n";
     return 0;
         
 
