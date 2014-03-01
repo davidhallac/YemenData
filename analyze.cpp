54a1a601e1712b888db7f8abd37acc7983efdeb3
David Hallac <david.hallac@gmail.com>
2014-02-28 19:59:38 -0800

    Streamlined Code

--- analyze.cpp
+++ analyze.cpp
@@ -352,60 +352,87 @@ int main(int argc, const char * argv[])
 
 
 
-
-
-
-
-
-	//START TIMES MAY 24th 2010
-	/*int counter = 0;
+	//STATISTICS FOR SPECIFIC TOWER - fill in tower IDs, select call type
+	int counter = 0;
 	int thresholds [] = {0, 1500, 3000, 4500, 10000, 11500, 13000, 14500, 20000, 21500, 23000, 24500, 30000, 31500, 33000, 34500, 40000, 41500, 43000, 44500, 50000, 51500, 53000, 54500, 60000, 61500, 63000, 64500, 70000, 71500, 73000, 74500, 80000, 81500, 83000, 84500, 90000, 91500, 93000, 94500, 100000, 101500, 103000, 104500, 110000, 111500, 113000, 114500, 120000, 121500, 123000, 124500, 130000, 131500, 133000, 134500, 140000, 141500, 143000, 144500, 150000, 151500, 153000, 154500, 160000, 161500, 163000, 164500, 170000, 171500, 173000, 174500, 180000, 181500, 183000, 184500, 190000, 191500, 193000, 194500, 200000, 201500, 203000, 204500, 210000, 211500, 213000, 214500, 220000, 221500, 223000, 224500, 230000, 231500, 233000, 234500, 240000};
-	uint totals [96] = {0};
-	
+	TVec<TPhoneCall> totals [96];
+
 	for (int i=0; i < PhoneLoad.Len(); i++) 
 	{
 		//Look at calls only
 		if(PhoneLoad[i].getDuration() >= 1)
 		{
-			//Uncomment the selection you want to analyze
-			//Tower 1, outgoing
-			if (strncmp (PhoneLoad[i].getLocSrc().CStr(), "4210275302948", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "4210275302949", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "421027530294A", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102A0282948", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102A0282949", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102A028294A", 13) == 0)
-			{//2) Tower 1, incoming
-			if (strncmp (PhoneLoad[i].getLocDest().CStr(), "4210275302948", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "4210275302949", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "421027530294A", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102A0282948", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102A0282949", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102A028294A", 13) == 0)
-			//3) Tower 1, both directions
-			//if (strncmp (PhoneLoad[i].getLocDest().CStr(), "4210275302948", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "4210275302949", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "421027530294A", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102A0282948", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102A0282949", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102A028294A", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "4210275302948", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "4210275302949", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "421027530294A", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102A0282948", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102A0282949", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102A028294A", 13) == 0)
-			//4) Tower 2, both directions
-			//if (strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102753028CF", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102753028D0", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102753028D1", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102753028D2", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102A02828CF", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102A02828D0", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102A02828D1", 13) == 0 || strncmp (PhoneLoad[i].getLocSrc().CStr(), "42102A02828D2", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102753028CF", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102753028D0", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102753028D1", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102753028D2", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102A02828CF", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102A02828D0", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102A02828D1", 13) == 0 || strncmp (PhoneLoad[i].getLocDest().CStr(), "42102A02828D2", 13) == 0)
+
+			//Fill in vector of tower IDs
+			TVec<TStr> towerIDs;
+			towerIDs.Add("4210275302948");
+			towerIDs.Add("4210275302949");
+			towerIDs.Add("421027530294A");
+			towerIDs.Add("42102A0282948");
+			towerIDs.Add("42102A0282949");
+			towerIDs.Add("42102A028294A");
+
+
+
+			int meetsCondition = 0;
+			for (int j=0; j < towerIDs.Len(); j++)
+			{
+				//1) Outgoing
+				if(strncmp(PhoneLoad[i].getLocSrc().CStr(), towerIDs[j].CStr(), 13) == 0)
+				//2) Incoming
+				//if(strncmp(PhoneLoad[i].getLocDest().CStr(), towerIDs[j], 13) == 0)
+				//3) Either
+				//if(strncmp(PhoneLoad[i].getLocSrc().CStr(), towerIDs[j], 13) == 0 || strncmp(PhoneLoad[i].getLocDest().CStr(), towerIDs[j], 13) == 0)
+				//4) Both (call stays within tower)
+				//if(strncmp(PhoneLoad[i].getLocSrc().CStr(), towerIDs[j], 13) == 0 && strncmp(PhoneLoad[i].getLocDest().CStr(), towerIDs[j], 13) == 0)
+				{
+					meetsCondition = 1;
+					break;
+				}
+			}
+			if(meetsCondition)
 			{
 				counter++;
-				for(int j=0; j < (sizeof(thresholds)/sizeof(int)); j++)
+				for(int j = 0; j < 96; j++)
 				{
 					if (PhoneLoad[i].getTime() >= thresholds[j] && PhoneLoad[i].getTime() < thresholds[j+1])
 					{
-						totals[j] = totals[j] + 1;
+						totals[j].Add(PhoneLoad[i]);
 						break;
 					}
 				}
-			}}
+			}
+
 		}
 	}
-
+	//Now, analyze TVecs, broken down by start time
 	char subbuff[9];
 	memcpy( subbuff, &(argv[1])[13], 8 );
 	subbuff[8] = '\0';
 	cout << subbuff << "\n";
-	//Write marginals to file (append)
 	ofstream fileout;
 	fileout.open("startTimes.csv", ios::app);
 	fileout << subbuff;
 	fileout << ",";
 	fileout << counter;
-	for (int j = 0; j < 96; j++)
+	for (int i=0; i < 96; i++)
 	{
+		//WriteVal is what you write to file
+		int writeVal = 0;
+
+		//Average duration
+
+
+		//Average distance
+
+
+		//Number
+		writeVal = totals[i].Len();
+
 		fileout << ",";
-		fileout << totals[j];
+		fileout << writeVal;
 	}
-	fileout << "\n";*/
+	fileout << "\n";
 
 
 
@@ -498,7 +525,7 @@ int main(int argc, const char * argv[])
 
 
 	//INTERNATIONAL CALL STATISTICS
-	int total = PhoneLoad.Len();
+	/*int total = PhoneLoad.Len();
 	int counterIn = 0;
 	int counterOut = 0;	
 	for (int i=0; i < total; i++) 
@@ -548,7 +575,10 @@ int main(int argc, const char * argv[])
 	fileout << counterIn;
 	fileout << ",";
 	fileout << counterOut;
-	fileout << "\n";
+	fileout << "\n";*/
+
+
+
 
 }
 
