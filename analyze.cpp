a38eec27dd04aa549e340542c9c068f2a6f028b8
David Hallac <david.hallac@gmail.com>
2014-03-01 20:56:21 -0800

    Bug Fix

--- analyze.cpp
+++ analyze.cpp
@@ -352,8 +352,8 @@ int main(int argc, const char * argv[])
 
 
 
-	//STATISTICS FOR SPECIFIC TOWER - fill in tower IDs, 
-	//select call type, and choose what you want to print
+	//STATISTICS FOR SPECIFIC TOWER - 3 things to select:
+	//tower IDs, call type, and what you want to print to csv
 	int counter = 0;
 	int thresholds [] = {0, 1500, 3000, 4500, 10000, 11500, 13000, 14500, 20000, 21500, 23000, 24500, 30000, 31500, 33000, 34500, 40000, 41500, 43000, 44500, 50000, 51500, 53000, 54500, 60000, 61500, 63000, 64500, 70000, 71500, 73000, 74500, 80000, 81500, 83000, 84500, 90000, 91500, 93000, 94500, 100000, 101500, 103000, 104500, 110000, 111500, 113000, 114500, 120000, 121500, 123000, 124500, 130000, 131500, 133000, 134500, 140000, 141500, 143000, 144500, 150000, 151500, 153000, 154500, 160000, 161500, 163000, 164500, 170000, 171500, 173000, 174500, 180000, 181500, 183000, 184500, 190000, 191500, 193000, 194500, 200000, 201500, 203000, 204500, 210000, 211500, 213000, 214500, 220000, 221500, 223000, 224500, 230000, 231500, 233000, 234500, 240000};
 	TVec<TPhoneCall> totals [96];
@@ -364,7 +364,7 @@ int main(int argc, const char * argv[])
 		if(PhoneLoad[i].getDuration() >= 1)
 		{
 
-			//Fill in vector of tower IDs
+			//FILL IN TOWER IDS WITH TOWERS TO ANALYZE
 			TVec<TStr> towerIDs;
 			towerIDs.Add("4210275302948");
 			towerIDs.Add("4210275302949");
@@ -378,7 +378,8 @@ int main(int argc, const char * argv[])
 			int meetsCondition = 0;
 			for (int j=0; j < towerIDs.Len(); j++)
 			{
-				//1) Outgoing
+				//UNCOMMENT THE OPTION YOU WANT
+				//1) Outgoing calls
 				if(strncmp(PhoneLoad[i].getLocSrc().CStr(), towerIDs[j].CStr(), 13) == 0)
 				//2) Incoming
 				//if(strncmp(PhoneLoad[i].getLocDest().CStr(), towerIDs[j], 13) == 0)
@@ -419,19 +420,26 @@ int main(int argc, const char * argv[])
 	for (int i=0; i < 96; i++)
 	{
 		//WriteVal is what you write to file
-		int totalSum = 0;
 		int writeVal = 0;
+
+		//UNCOMMENT THE SELECTION YOU WANT
 		//Average duration
+		int totalSum = 0;
+		int counterSum = 0;
 		if (totals[i].Len() > 0)
 		{
 			for(int j = 0; j < totals[i].Len(); j++)
 			{
-				totalSum += totals[i][j].getDuration();
+				if(totals[i][j].getDuration() < 1799)
+				{
+					totalSum += totals[i][j].getDuration();
+					counterSum++;	
+				}
 
 			}
 		}
-		if(totals[i].Len() != 0)
-			writeVal = totalSum / totals[i].Len();
+		if(counterSum != 0)
+			writeVal = totalSum / counterSum;
 		//Average distance
 
 
